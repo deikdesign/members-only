f005f1ab6d73a9478f736953b0834021dd6c4de6
Janluc <Janlucsaneaux@gmail.com>
2021-02-09 18:55:47 -0400

    Fix styling according linter

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -36,6 +36,7 @@ class PostsController < ApplicationController
   end
 
   private
+
   # Use callbacks to share common setup or constraints between actions.
   def set_post
     @post = Post.find(params[:id])
